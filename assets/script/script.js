8eaa1dbd7989351e040b19a2977f8dc187b14f93
Hungyiu Wong <hungyiu2009@live.hk>
2021-08-10 09:29:58 -0700

    revised codes 8/10

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,36 @@
-
-var specialChar = ["@", "$", "#", "&", "*", "!"]
-var numberChoi = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lowerCase = "a b c d f g h i j k l m n o p q r m n o p q r s t u v w x y z";
-var lowerCaseEl = lowerCase .split(" ");
-var upperCase = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
-var upperCaseEl = upperCase .split (" ");
-
+var specialCharArr = ["@", "$", "#", "&", "*", "!"];
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCase = "abcdefghijklmnopqrmnopqrstuvwxyz";
+var lowerCaseArr = lowerCase.split("");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperCaseArr = upperCase.split("");
 
 function getUserOperations() {
-  var length = prompt("Please select the length of the password")
-  if (length < 8 || length >128) {
-    alert("Please ensure the password length is between 8 characters to 128 characters");
+  var length = prompt("Please select the length of the password");
+  if (length < 8 || length > 128) {
+    alert(
+      "Please ensure the password length is between 8 characters to 128 characters"
+    );
     return;
-  } 
-  var useLowerCase = confirm ("Would you like to use lowercase characters in your password?");
-  var useUpperCase = confirm ("Would you like to use uppercase characters in your password?");
-  var useNumbers = confirm ("Would you like to use numbers in your password?");
-  var useSpecialChar = confirm ("Would you like to use special characters in your password?");
- 
-  console.log (useLowerCase,useNumbers, useUpperCase, useSpecialChar);
-  if (useLowerCase===false && useUpperCase === false && useNumbers === false && useSpecialChar === false) {
+  }
+  var useLowerCase = confirm(
+    "Would you like to use lowercase characters in your password?"
+  );
+  var useUpperCase = confirm(
+    "Would you like to use uppercase characters in your password?"
+  );
+  var useNumbers = confirm("Would you like to use numbers in your password?");
+  var useSpecialChar = confirm(
+    "Would you like to use special characters in your password?"
+  );
+
+  console.log(useLowerCase, useNumbers, useUpperCase, useSpecialChar);
+  if (
+    useLowerCase === false &&
+    useUpperCase === false &&
+    useNumbers === false &&
+    useSpecialChar === false
+  ) {
     alert("Please select at least one element for your password");
     return;
   }
@@ -30,47 +41,59 @@ function getUserOperations() {
     useNumbers: useNumbers,
     useSpecialChar: useSpecialChar,
     length: length,
-  }
+  };
 
   return userChoices;
 }
 
-function generatePassword () {
- var input = getUserOperations();
- var results = userChoices();
- var userPosChars = [];
- var guarChar = [];
+function generatePassword() {
+  var input = getUserOperations();
+  var password = "";
+  console.log(input);
+  
+  //create a big pool of array with all user selected char sets.
+  var userPool = [];
+  //How to copy user selected array?
 
- if (input.useLowerCase) {
-   userPosChars = userPosChars.concat(useLowerCase)
-   guarChars.push(getRandom(useLowerCase))
- }
- if (input.useUpperCase) {
-   userPosChars = userPosChars.concat(userUpperCase)
-   guarChars.push(getRandom(useUpperCase))
- }
- if (input.useNumber) {
-   userPosChars = userPosChars.concat(useNumber)
-   guarChars.push(getRandom(useNumber))
- }
- if (input.useSpecialChar) {
-   userPosChars = userPosChar.concat(useSpecialChar)
-   guarChars.push(getRandom(useSpecialChar))
- }
- }
+  if (input.useLowerCase) {
+    userPool = userPool.concat(lowerCaseArr);
+  }
+  console.log(userPool);
 
- var generateBtn = document.querySelector("#generate");
+  if (input.useUpperCase) {
+    userPool = userPool.concat(upperCaseArr);
+  }
+  console.log(userPool);
 
- function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+  if (input.useNumbers) {
+    userPool = userPool.concat(numberArr);
+  }
+  console.log(userPool);
 
-     passwordText.value = password;
+  if (input.useSpecialChar) {
+    userPool = userPool.concat(specialCharArr);
+  }
+  console.log(userPool);
+
+  //concat random characters to the password string.
+  for (var i = 0; i < input.length; i++) {
+    password = password + userPool[Math.floor(Math.random() * userPool.length)];
+  }
+  console.log(password);
+  return password;
+}
+
+//use for loop to go through the big pool
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
 generateBtn.addEventListener("click", writePassword);
 
 
-for (var i=0; i< length; i++) {
-  userChoices.push (password[Math.floor(Math.random() * guarChars.length)]);
-}
